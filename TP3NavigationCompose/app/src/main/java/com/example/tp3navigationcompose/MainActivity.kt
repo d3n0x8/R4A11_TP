c95d02aeeed235b5a347075cdf4020b66901d91e
Andreas Rey Malissein <andreasrey87@gmail.com>
2025-02-04 10:59:07 +0100

    add name form

--- TP3NavigationCompose/app/src/main/java/com/example/tp3navigationcompose/MainActivity.kt
+++ TP3NavigationCompose/app/src/main/java/com/example/tp3navigationcompose/MainActivity.kt
@@ -8,12 +8,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -70,6 +76,7 @@ fun AppNavigation() {
 
 @Composable
 fun FormScreen(navController: NavHostController) {
+    var name by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -79,6 +86,14 @@ fun FormScreen(navController: NavHostController) {
     ) {
         Text(text = "Page du formulaire.", style = MaterialTheme.typography.titleMedium)
         Spacer(modifier = Modifier.height(24.dp))
+        TextField(
+            value = name,
+            onValueChange = { newText -> name = newText },
+            label = { Text("Entrez votre nom") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
         Button(onClick = { navController.popBackStack() }) { Text(text = "Retour") }
     }
 }
